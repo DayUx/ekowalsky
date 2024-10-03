c4993d7fecb312aaf51d4b3f2cb5ecae1436099a
dayux <paulcoignac@gmail.com>
2024-10-03 15:03:32 +0200

    test

--- ekowalsky-front/src/components/LogIn/LogIn.js
+++ ekowalsky-front/src/components/LogIn/LogIn.js
@@ -58,6 +58,7 @@ export default function LogIn() {
                     navigate('/');
                 } else {
                     toast.error(res.data.message);
+                    fields.password = '';
                 }
             }));
     };
